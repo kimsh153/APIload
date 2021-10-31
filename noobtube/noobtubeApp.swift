851f6ac2178ebbf85d9d42595d5766298a898bd7
kimsh153 <sunghun4777@gmail.com>
2021-10-31 17:07:13 +0900

    Initial Commit

---
+++ noobtube/noobtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  noobtubeApp.swift
+//  noobtube
+//
+//  Created by gsm10 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct noobtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
